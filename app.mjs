e43891c2d864d681d8ba43071f41adf53217e40b
ayeshaqadri <qadria392@gmail.com>
2025-02-24 20:32:53 +0500

    addserver

--- app.mjs
+++ app.mjs
@@ -4,7 +4,7 @@ const app = express();
 const port = 30000;
 
 app.get("/", (req, res) => {
-  res.send("This is ayesha port ");
+  res.send("This is ayesha qadri port ");
 });
 
 app.listen(port, () => {
---
+++ web/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1>My api</h1>
+      <form onSubmit="">
+        <input type="text" />
+        <button type='submit'>Press me</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
